8cdec773a02954fb81f0ff6c16dc47afe6a1cc49
Christian Engwer <christi@dune-project.org>
2015-03-19 11:33:42 +0100

    [test] implement a test for a multi component function

--- dune/functions/common/test/differentiablefunctiontest.cc
+++ dune/functions/common/test/differentiablefunctiontest.cc
@@ -15,6 +15,51 @@
 #include <dune/functions/analyticfunctions/polynomial.hh>
 #include <dune/functions/analyticfunctions/trigonometricfunction.hh>
 
+template<int V>
+class ConstantIntegerFunction
+{
+public:
+  double operator() (const double& x, const double& t) const
+  {
+    return V;
+  }
+
+  template<int D=1>
+  friend ConstantIntegerFunction<0> derivative(const ConstantIntegerFunction& p,
+    Dune::Functions::DerivativeDirection<D> = Dune::Functions::DerivativeDirection<D>())
+  {
+    return ConstantIntegerFunction<0>();
+  }
+
+  // friend ConstantIntegerFunction<0> derivative(const ConstantIntegerFunction& p)
+  // {
+  //   return ConstantIntegerFunction<0>();
+  // }
+};
+
+class MultiParamTestFunction
+{
+public:
+
+  double operator() (const double& x, const double& t) const
+  {
+    return x-t;
+  }
+
+  friend ConstantIntegerFunction<1> derivative(const MultiParamTestFunction& p,
+    Dune::Functions::DerivativeDirection<1> = Dune::Functions::DerivativeDirection<1>())
+  {
+    return ConstantIntegerFunction<1>();
+  }
+
+  friend ConstantIntegerFunction<-1> derivative(const MultiParamTestFunction& p,
+    Dune::Functions::DerivativeDirection<2>)
+  {
+    return ConstantIntegerFunction<-1>();
+  }
+};
+
+
 //#include <dune/functions/common/callable.hh>
 
 //#include "derivativecheck.hh"
@@ -24,9 +69,13 @@
 struct DifferentiableFunctionImplementableTest
 {
 
-  template<class F>
-  static bool checkWithFunction(F&& f)
+  template<typename Range, typename... Domain, class F, int DiffDir, typename... Params>
+  static bool checkWithFunction(Dune::Functions::SignatureTag<Range(Domain...)>, F&& f,
+    Dune::Functions::DerivativeDirection<DiffDir> diffDir,
+    Params... params)
   {
+    static_assert(sizeof...(Domain) == sizeof...(Params), "Number of parameters does not math the signature");
+
     bool passed = true;
 
     {
@@ -35,7 +84,7 @@ struct DifferentiableFunctionImplementableTest
 //      passed = passed and DerivativeCheck<QuadraticPolynomial>::checkAllImplementedTrulyDerived(testFunction, 10);
 
       // Test whether I can evaluate the function somewhere
-      std::cout << "Function value at x=5: " << f(5) << std::endl;
+      std::cout << "Function value at x=5: " << f(params...) << std::endl;
 
 
 
@@ -43,50 +92,64 @@ struct DifferentiableFunctionImplementableTest
 
       // Test whether I can evaluate the first derivative
       auto df = derivative(f);
-      std::cout << "Derivative at x=5: " << df(5) << std::endl;
+      std::cout << "Derivative at x=5: " << df(params...) << std::endl;
 
       // Test whether I can evaluate the second derivative through FunctionHandle
       auto ddf = derivative(df);
-      std::cout << "Second derivative at x=5: " << ddf(5) << std::endl;
+      std::cout << "Second derivative at x=5: " << ddf(params...) << std::endl;
 
       // Test whether I can evaluate the third derivative through FunctionHandle
       auto dddf = derivative(ddf);
-      std::cout << "Third derivative at x=5: " << dddf(5) << std::endl;
+      std::cout << "Third derivative at x=5: " << dddf(params...) << std::endl;
 
+      std::cout << std::endl << "Check calling derivatives with explicit derivative direction" << std::endl;
 
+      // Test whether I can evaluate the derivative via direction parameter
+      auto dfdn = derivative(f,diffDir);
+      std::cout << "Derivative at x=5: " << dfdn(params...) << std::endl;
+
+      // // Test whether I can evaluate the second derivative through FunctionHandle
+      // auto ddfdn = derivative(df,diffDir);
+      // std::cout << "Second derivative at x=5: " << ddfdn(params...) << std::endl;
+
+      // // Test whether I can evaluate the third derivative through FunctionHandle
+      // auto dddfdn = derivative(ddf,diffDir);
+      // std::cout << "Third derivative at x=5: " << dddfdn(params...) << std::endl;
 
       std::cout << std::endl << "Check calling derivatives through DifferentiableFunction object" << std::endl;
-      Dune::Functions::DifferentiableFunction<double(const double&)> fi = f;
+      Dune::Functions::DifferentiableFunction<Range(Domain...)> fi = f;
 
       // Try to reassign wrapper
       fi = f;
 
       // Try assigning a default constructed wrapper
-      Dune::Functions::DifferentiableFunction<double(const double&)> fii;
+      Dune::Functions::DifferentiableFunction<Range(Domain...)> fii;
       fii = fi;
 
       // Try to copy wrapper
       auto fiii = fii;
 
-      std::cout << "Function value at x=5: " << fiii(5) << std::endl;
+      std::cout << "Function value at x=5: " << fiii(params...) << std::endl;
       // Test whether I can evaluate the first derivative
       auto dfiii = derivative(fiii);
-      std::cout << "Derivative at x=5: " << dfiii(5) << std::endl;
+      std::cout << "Derivative at x=5: " << dfiii(params...) << std::endl;
 
       // Test whether I can evaluate the second derivative through FunctionHandle
       auto ddfiii = derivative(dfiii);
-      std::cout << "Second derivative at x=5: " << ddfiii(5) << std::endl;
+      std::cout << "Second derivative at x=5: " << ddfiii(params...) << std::endl;
 
       // Test whether I can evaluate the third derivative through FunctionHandle
       auto dddfiii = derivative(ddfiii);
-      std::cout << "Third derivative at x=5: " << dddfiii(5) << std::endl;
+      std::cout << "Third derivative at x=5: " << dddfiii(params...) << std::endl;
 
       // Wrap as non-differentiable function
-      Dune::Functions::DifferentiableFunction<double(const double&)> g = [=] (const double& x) {return f(x);};
-      std::cout << "Function value at x=5: " << g(5) << std::endl;
+      Dune::Functions::DifferentiableFunction<Range(Domain...)> g = [=] (Domain... x) {return f(x...);};
+      std::cout << "Function value at x=5: " << g(params...) << std::endl;
 
       try {
         auto dg = derivative(g);
+        // auto dg2 = derivative(g,diffDir);
+        // std::cout << dg(params...) << " ... " << dg2(params...) << std::endl;
         passed = false;
       }
       catch (Dune::NotImplemented e)
@@ -119,16 +182,31 @@ struct DifferentiableFunctionImplementableTest
   {
     bool passed = true;
 
-    passed = passed and checkWithFunction(Dune::Functions::Polynomial<double>({1, 2, 3}));
-    passed = passed and checkWithFunction(Dune::Functions::TrigonometricFunction<double, 1, 0>());
+    double value = 5.5;
+    Dune::Functions::SignatureTag<double(const double&)> signature;
+
+    using Dune::Functions::derivativeDirection::_d1;
+    using Dune::Functions::derivativeDirection::_d2;
+
+    passed = passed and checkWithFunction(signature, Dune::Functions::Polynomial<double>({1, 2, 3}), _d1, value);
+    passed = passed and checkWithFunction(signature, Dune::Functions::TrigonometricFunction<double, 1, 0>(), _d1, value);
 
     auto f = [](double x){ return std::sin(x);};
     auto df = [](double x){ return std::cos(x);};
     auto ddf = [](double x){ return -std::sin(x);};
     auto dddf = [](double x){ return -std::cos(x);};
 
-    auto F = makeDifferentiableFunctionFromCallables(Dune::Functions::SignatureTag<double(double)>(), f, df, ddf, dddf);
-    passed = passed and checkWithFunction(F);
+    auto F = makeDifferentiableFunctionFromCallables(signature, f, df, ddf, dddf);
+    passed = passed and checkWithFunction(signature, F, _d1, value);
+
+    Dune::Functions::SignatureTag<double(const double&, const double&)> signature2;
+    // auto f2 = [](double x, double t){ return x-t; };
+    // auto df2dx = [](double x, double t){ return 1; };
+    // auto df2dt = [](double x, double t){ return -1; };
+    // auto F2 = makeDifferentiableFunctionFromCallables(signature2, f2, df2dx);
+    MultiParamTestFunction F2;
+    passed = passed and checkWithFunction(signature2, F2, _d1, 1.0, 2.0);
+    passed = passed and checkWithFunction(signature2, F2, _d2, 1.0, 2.0);
 
     return passed;
   }

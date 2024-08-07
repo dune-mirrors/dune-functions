979a7197a55264404f22f819cde0431b77d9bf51
Simon Praetorius <simon.praetorius@tu-dresden.de>
2024-08-07 18:16:13 +0200

    Update the SPDX header

--- dune/functions/functionspacebases/basix/utility.hh
+++ dune/functions/functionspacebases/basix/utility.hh
@@ -1,5 +1,9 @@
-// SPDX-FileCopyrightText: Copyright © DUNE Project contributors, see file LICENSE.md in module root
-// SPDX-License-Identifier: LicenseRef-GPL-2.0-only-with-DUNE-exception
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+
+// SPDX-FileCopyrightText: Copyright © DUNE Project contributors, see file AUTHORS.md
+// SPDX-License-Identifier: LicenseRef-GPL-2.0-only-with-DUNE-exception OR LGPL-3.0-or-later
+
 #ifndef DUNE_FUNCTIONS_BASIX_UTILITY_HH
 #define DUNE_FUNCTIONS_BASIX_UTILITY_HH
 

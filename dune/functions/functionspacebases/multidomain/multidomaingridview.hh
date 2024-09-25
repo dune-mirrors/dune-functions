e14699bbc57da6fd3d196cfa97001937d43ac78f
Christian Engwer <christi@dune-project.org>
2024-09-25 21:34:08 +0200

    [multidomain] first version of multidomain support
    We can now create the correct preBasis objects, but up to now, we only
    test a small set of the expected basis properties.

---
+++ dune/functions/functionspacebases/multidomain/multidomaingridview.hh
@@ -0,0 +1,117 @@
+#ifndef DUNE_FUNCTIONS_MULTIDOMAIN_MULTIDOMAINGRIDVIEW_HH
+#define DUNE_FUNCTIONS_MULTIDOMAIN_MULTIDOMAINGRIDVIEW_HH
+
+#include <memory>
+#include <type_traits>
+#include <utility>
+
+#include "restrictedgridview.hh"
+#include "restrictedindexset.hh"
+
+namespace Dune::Functions::MultiDomain {
+
+template <class GridView>
+class MultiDomainGridView : public GridView
+{
+public:
+  using HostGridView = GridView;
+  using SubdomainGridView = RestrictedGridView<GridView>;
+  using SubdomainIndexSet = typename SubdomainGridView::IndexSet;
+
+  MultiDomainGridView(const GridView & gridView, const std::shared_ptr<PartitionedDomainInfo> & domainInfo) :
+    GridView(gridView),
+    // _cellMapper(gridView, mcmgElementLayout()),
+    // _entityMapper(gridView, [](GeometryType, int) { return true; }),
+    _cellMapper((const GridView&)(*this), mcmgElementLayout()),
+    // here we might optimize and setup only required indices
+    _entityMapper((const GridView&)(*this), [](GeometryType, int) { return true; }),
+    _domainInfo(domainInfo)
+  {
+    constexpr int dim = GridView::dimension;
+    std::size_t subdomains = domainInfo->subdomains().size();
+
+    // create a temporary map from partition index to subdomain index
+    std::map<int,std::vector<int>> parts;
+    for (int d = 0; d<subdomains; d++)
+      for (int p : domainInfo->subdomain(d))
+        parts[p].push_back(d);
+
+    // clear and initialize indices and sizes
+    _sizes.resize(subdomains);
+    _indices.resize(subdomains);
+    for (int dom=0; dom<subdomains; dom++)
+    {
+      _indices[dom].resize(_entityMapper.size(), -1);
+      _sizes[dom].clear();
+      for (unsigned int d = 0; d <= dim; d++)
+      {
+        for (int t=0; t<LocalGeometryTypeIndex::size(d); t++)
+        {
+          auto gt = LocalGeometryTypeIndex::type(d,t);
+          _sizes[dom][gt] = 0;
+        }
+      }
+    }
+
+    // loop through mesh, check sub-domain and update indices
+    for (auto && e : elements(gridView))
+    {
+      auto i = _cellMapper.index(e);
+      auto refcell = referenceElement<double,dim>(e.type());
+      auto p = _domainInfo->partition(i);
+      for (int subdomain : parts[p])
+      {
+        // every subentity is part of the subdomain
+        for (int codim = 0; codim<=dim; codim++)
+          // n'th subentity of codim
+          for (int n = 0; n<refcell.size(codim); n++)
+          {
+            // use the next unused index within this geometry type
+            auto gt = refcell.type(n, codim);
+            auto j = _entityMapper.subIndex(e,n,codim);
+            // set new index, if not already set
+            if (_indices[subdomain][j] == -1)
+              _indices[subdomain][j] = _sizes[subdomain][gt]++;
+          }
+      }
+    }
+
+    // setup indexSets
+    _indexSets.resize(0);
+    for (int d=0; d<subdomains; d++)
+      _indexSets.push_back(std::make_shared<SubdomainIndexSet>(
+          *this, _entityMapper,
+          _indices[d], _sizes[d]));
+
+    // seup gridViews
+    _gridViews.resize(0);
+    for (int d=0; d<subdomains; d++)
+      _gridViews.push_back(std::make_shared<SubdomainGridView>(
+          *this, _indexSets[d]));
+  }
+
+  /** \brief obtain the index set
+   *
+   * The lifetime of the returned index set is bound to the lifetime of the
+   * grid view. Keep a copy of the grid view to prevent the index set from
+   * becoming a dangling reference.
+   */
+  const SubdomainGridView & subdomainGridView (int i) const
+  {
+    assert(i < _gridViews.size());
+    return *_gridViews[i];
+  }
+
+private:
+  MultipleCodimMultipleGeomTypeMapper<GridView> _cellMapper;
+  MultipleCodimMultipleGeomTypeMapper<GridView> _entityMapper;
+  std::shared_ptr<PartitionedDomainInfo> _domainInfo;
+  std::vector<std::map<GeometryType,int>> _sizes;
+  std::vector<std::vector<int>> _indices;
+  std::vector<std::shared_ptr<SubdomainIndexSet>> _indexSets;
+  std::vector<std::shared_ptr<SubdomainGridView>> _gridViews;
+};
+
+}
+
+#endif // DUNE_FUNCTIONS_MULTIDOMAIN_MULTIDOMAINGRIDVIEW_HH
---
+++ dune/functions/functionspacebases/test/multidomaintest.cc
@@ -0,0 +1,74 @@
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+#include <iostream>
+#include <dune/common/parallel/mpihelper.hh>
+#include <dune/functions/functionspacebases/defaultglobalbasis.hh>
+#include <dune/functions/functionspacebases/lagrangebasis.hh>
+#include <dune/functions/functionspacebases/compositebasis.hh>
+#include <dune/functions/functionspacebases/powerbasis.hh>
+#include <dune/grid/yaspgrid.hh>
+#include <dune/functions/functionspacebases/multidomain.hh>
+
+int main(int argc, char** argv)
+{
+  using namespace Dune::Functions::BasisFactory;
+  using namespace Dune::Functions::MultiDomain;
+
+  Dune::MPIHelper::instance(argc, argv);
+
+  constexpr int dim = 2;
+  Dune::YaspGrid<dim> grid{ {1.0,1.0}, {3,1} };
+  const auto & gridView = grid.leafGridView();
+
+  std::vector<int> partitions(grid.size(0), 0);
+  auto const& indexSet = grid.leafIndexSet();
+  for (auto const& e : elements(grid.leafGridView())) {
+    auto center = e.geometry().center();
+    partitions[indexSet.index(e)] =
+      center[0] < 0.45 ? 0 :
+      (center[0] > 0.55 ? 1 : 2);
+  }
+  // partitions 0,2,1 ... 0: domain 0, 1: domain 1, 2: overlap
+  // -> subdomains domain0 = {0,2}, domain1 = {1,2}, interface = {2}
+  auto domainInfo = createPartitionedDomainInfo(std::move(partitions), {{0,2}, {1,2}, {2}});
+  // CutCellDomainInfo domainInfo(levelsets, {{0,2}, {1,2}, {2}});
+
+
+  auto stokesPreBasis = restrict(composite(power<dim>(lagrange<2>()), lagrange<1>()), subdomain(0));
+  // we definitely
+  auto darcyPreBasis = restrict(lagrange<1>(), subdomain(1));
+  auto multiDomainPreBasis = multiDomainComposite(domainInfo, stokesPreBasis, darcyPreBasis);
+  auto fooDomainPreBasis = multiDomainComposite(domainInfo, darcyPreBasis);
+  // auto basis = makeBasis(gridView,fooDomainPreBasis);
+  auto basis = makeBasis(gridView,multiDomainPreBasis);
+
+  basis.gridView(); // -> original gridView
+  auto localView = basis.localView();
+  int i = 0;
+  for (auto && e : elements(gridView))
+  {
+    localView.bind(e); // -> domainInfo.bind(e), dann child.bind(e)
+    std::cout << "cell " << i++
+              << " has " << localView.size() << " DOFs" << std::endl;
+  }
+
+  using namespace Dune::Indices;
+  std::cout << "DOM0: " << basis.preBasis().subPreBasis(_0).gridView().size(0) << " cells\n";
+  std::cout << "DOM1: " << basis.preBasis().subPreBasis(_0).gridView().size(0) << " cells\n";
+  std::cout << "DOM0: " << basis.preBasis().subPreBasis(_0).gridView().size(1) << " edges\n";
+  std::cout << "DOM1: " << basis.preBasis().subPreBasis(_0).gridView().size(1) << " edges\n";
+  std::cout << "DOM0: " << basis.preBasis().subPreBasis(_0).gridView().size(2) << " vertices\n";
+  std::cout << "DOM1: " << basis.preBasis().subPreBasis(_0).gridView().size(2) << " vertices\n";
+
+  // after setting a subdomain id for all elements, the number of DOFs has changed
+  // 3x1 mesh
+  // subdomains : 2x1 mesh -> 2 cells, 7 edges, 6 vertices
+  // TH(dom0)   :
+  //         v  :  30 DOFs ( 2 per cell, edge, vertex)
+  //         p  :   6 DOFs
+  // Lag(dom1)  :   6 DOFs
+  // total      :  42 DOFs
+  int sz = basis.dimension();
+  std::cout << "Functionsspace has " << sz << " global DOFs" << std::endl;
+}
